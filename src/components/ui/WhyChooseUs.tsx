75991df88d790a8a7df2c04f690097637bdaaa25
DHRUB DAS <dhrub27das@gmail.com>
2024-04-04 00:48:13 +0530

    Task Done

---
+++ src/components/ui/WhyChooseUs.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { StickyScroll } from "../ui/sticky-scroll-reveal";
+import Image from "next/image";
+
+function WhyChooseUs() {
+    return (
+        <div>WhyChooseUs</div>
+    )
+}
+
+export default WhyChooseUs
