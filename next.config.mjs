947214fe58c6166e13339638ea09107abef8a0e7
DHRUB DAS <dhrub27das@gmail.com>
2024-04-04 21:32:19 +0530

    Instructor Comp Done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com', 'res.cloudinary.com']
+    }
+    // unknown source se images ko domains mai specify karna padega
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import FeaturedCourses from "@/components/FeaturedCourses"
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCard from "@/components/TestimonialCard";
 import UpComingWebinars from "@/components/UpComingWebinars";
+import Instructors from "@/components/Instructors";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <TestimonialCard />
       <UpComingWebinars />
+      <Instructors />
     </main>
   );
 }
